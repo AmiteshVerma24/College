9ee7da486f5863567d839d1c1c8a464543271d72
Amitesh Verma <vamitesh2409@gmail.com>
2023-05-18 00:32:39 +0530

    Bubble sort algorithm

---
+++ Algorithms/bubble_sort.c
@@ -0,0 +1,67 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <time.h>
+
+void bubble_sort(int a[], int s)
+{
+    for (int i = 0; i < s; i++)
+    {
+        for (int j = 0; j < s - i - 1; j++)
+        {
+            if (a[j] > a[j + 1])
+            {
+                int temp = a[j];
+                a[j] = a[j + 1];
+                a[j + 1] = temp;
+            }
+        }
+    }
+}
+
+void print_array(int a[], int s)
+{
+    printf("\n==============================================================================\n");
+    for (int i = 0; i < s; i++)
+    {
+        printf("%d, ", a[i]);
+    }
+    printf("\n==============================================================================\n");
+}
+
+void random_array(int a[], int s)
+{
+    int num;
+    
+    for (int i = 0; i < s; i++)
+    {
+        a[i] = rand() % s;
+    }
+}
+
+int main()
+{
+    clock_t start, end;
+    double total_t;
+    int size;
+    printf("\n==============================================================================\n");
+    printf("\nEnter the number of elements in the array:- ");
+    scanf("%d", &size);
+    int arr[size];
+    random_array(arr, size);
+    printf("\nArray before sorting is:- ");
+    print_array(arr, size);
+    start = clock();                                            
+    bubble_sort(arr, size);
+    end = clock();
+    printf("\nArray after sorting is:- ");
+    print_array(arr, size);
+    total_t = (end - start) / CLOCKS_PER_SEC;
+    printf("\nTime taken to sort array is: %lf", total_t);
+    printf("\n==============================================================================\n");
+}
+// The clock() returns the number of clock ticks passed since the execution of the program has started
+// Clock tics is the smallest interval in the computer's CPU based on which it passes data which is measured
+// from the internal quartz clock of the CPU
+// Now to convert to seconds we divide by clocks per second which is the number of clock ticks passes in a
+// second
